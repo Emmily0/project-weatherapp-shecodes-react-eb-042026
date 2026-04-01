8e4ed631607d15be5cf456e136aaf3552dfd4ac6
Emmily0 <81269634+Emmily0@users.noreply.github.com>
2026-04-02 01:42:04 +0200

    Added a space in footer

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
       <a href="https://www.shecodes.io/" target="_blank" rel="noreferrer" title="SheCodes WeatherApp React Project">SheCodes</a> 
   {""} React Project coded in 2026 by <a href="https://www.shecodes.io/graduates/15505-emily-berner" target="_blank" rel="noreferrer" title="Emily's SheCodes profile"><strong>Emily Berner</strong></a>
   , open-sourced on <a href="https://github.com/Emmily0/project-weatherapp-shecodes-react-eb-042026" target="_blank" rel="noreferrer" title="Emily's WeatherApp React Project on GitHub">GitHub</a> 
-  and hosted on <a href="https://projectweather-shecodesreact-eb042026.netlify.app/" target="_blank" rel="noreferrer" title="Emily's WeatherApp React Project on Netlify">Netlify</a> 
+  {""} and hosted on <a href="https://projectweather-shecodesreact-eb042026.netlify.app/" target="_blank" rel="noreferrer" title="Emily's WeatherApp React Project on Netlify">Netlify</a> 
   </footer>
 
     </div>
